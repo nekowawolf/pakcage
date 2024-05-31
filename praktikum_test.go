82c75f452e04aeccecfedba6bb50c73e9a07a5a3
nekowawolf <irukainu20@gmail.com>
2024-05-31 15:26:35 +0700

    update : db

--- praktikum_test.go
+++ praktikum_test.go
@@ -35,11 +35,11 @@ func TestInsertPresensi(t *testing.T) {
 	phonenumber := "6811110023231"
 	checkin := "masuk"
 	biodata := model.Karyawan{
-		Nama:         "Ruud Gullit",
-		Phone_number: "6284564562",
-		Jabatan:      "Football Player",
-		Jam_kerja:    []model.JamKerja{jamKerja1, jamKerja2},
-		Hari_kerja:   []string{"Senin", "Selasa"},
+		Nama:        "Ruud Gullit",
+		Phone_number: "628456456222222",
+		Jabatan:     "Football Player",
+		Jam_kerja:   []model.JamKerja{jamKerja1, jamKerja2},
+		Hari_kerja:  []string{"Senin", "Selasa"},
 	}
 	insertedID, err := module.InsertPresensi(module.MongoConn, "presensi", long, lat, lokasi, phonenumber, checkin, biodata)
 	if err != nil {
@@ -47,7 +47,6 @@ func TestInsertPresensi(t *testing.T) {
 	}
 	fmt.Printf("Data berhasil disimpan dengan id %s", insertedID.Hex())
 }
-
 func TestGetKaryawanFromPhoneNumber(t *testing.T) {
 	phonenumber := "68122221814"
 	biodata, err := module.GetKaryawanFromPhoneNumber(phonenumber, module.MongoConn, "presensi")
